a76af30661aea49ddf3a73b1253e14d8282f3ba0
AntoineOD <antoine141@outlook.com>
2024-03-28 12:34:38 -0400

    API CONNECTION

--- createAccount.js
+++ createAccount.js
@@ -1,15 +1,12 @@
 const mesRep = document.createElement("p");
 const body = document.getElementById("resForm")
 document.querySelector("#createSubmit").addEventListener("click", async () => {
-  console.log("test test test 1");
   const nom = document.getElementById("nomFamille").value;
   const prenom = document.getElementById("prenom").value;
   const dateNaissance = document.getElementById("dateNaissance").value;
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
   const username = document.getElementById("username").value;
-
-
   if (!nom || !prenom || !dateNaissance || !email || !password || !username) {
     mesRep.innerHTML = "Erreur dans le formulaire : information manquante"
     body.appendChild(mesRep)
--- routes.php
+++ routes.php
@@ -18,37 +18,6 @@ try {
    
 }
 
-get('/api/pokemons', function () {
-  global $pdo;
-
-  $results = $pdo->query('select * from pokemon;');
-  header('Content-Type: application/json');
-  echo json_encode($results->fetchAll());
-});
-
-get('/api/pokemons/types/$nom', function ($nom) {
-  global $pdo;
-
-  $sql = "select * from pokemon where type1 =:nom or type2 =:nom";
-  $results = $pdo->prepare($sql);
-  $results->execute(['nom' => $nom]);
-  header('Content-Type: application/json');
-  echo json_encode($results->fetchAll());
-});
-
-get('/api/pokemons/$id', function ($id) {
-  global $pdo;
-
-  $sql = "select * from pokemon where pokedex_number=:id";
-  $results = $pdo->prepare($sql);
-  $results->execute(['id' => $id]);
-  header('Content-Type: application/json');
-  echo json_encode($results->fetch());
-
-});
-
-get('projet1/api/ajouterCompte','index.php');
-
 post('/projet1/api/ajouterCompte', function (){
     global $pdo;
     $json = file_get_contents('php://input');
@@ -74,111 +43,33 @@ post('/projet1/api/ajouterCompte', function (){
     }
 });
 
+post('/projet1/api/connection', function (){
+  global $pdo;
+  $json = file_get_contents('php://input');
+  $data = json_decode($json, true);
+  $username = $data['username']??null;
+  $password = $data['password']??null;
+  if(empty($username)|| empty($password)){
+    echo json_encode(["message" => "ca marche"]);
+  }
+  else{
+    
+    $stmt = $pdo->prepare("SELECT * FROM EQ1_Compte WHERE username = ? AND password = ?");
+    $stmt->execute([$username, $password]);
+    if((bool)$stmt->fetchColumn();){
+      echo json_encode(["connection" => "vrai"]);
+    }
+    else{
+      echo json_encode(["connection" => "faux"]);
+    }
+  }
+});
+
 function emailExists($pdo, $email){
   $verifMail = $pdo->prepare("SELECT 1 FROM EQ1_Compte WHERE email=?");
   $verifMail->execute([$email]);
   return (bool)$verifMail->fetchColumn();
 }
 
-// Static GET
-// In the URL -> http://localhost
-// The output -> Index
-get('/api/test', 'monapi.php');
-get('/api/autretest', 'monapi.php');
 
-// Dynamic GET. Example with 1 variable
-// The $id will be available in user.php
-//api/user/4
-//api/user/autre
-get('/api/user/$mavariable', 'apidyn.php');
-
-// Dynamic GET. Example with 2 variables
-// The $name will be available in full_name.php
-// The $last_name will be available in full_name.php
-// In the browser point to: localhost/user/X/Y
-get('/user/$name/$last_name', 'views/full_name.php');
-
-// Dynamic GET. Example with 2 variables with static
-// In the URL -> http://localhost/product/shoes/color/blue
-// The $type will be available in product.php
-// The $color will be available in product.php
-get('/product/$type/color/$color', 'product.php');
-
-$info = [
-  [
-    "message" => "Je suis un tableau associatif",
-    "info" => [
-      "nombre" => 42,
-      "tableau" => [10, 20, 30]
-    ]
-  ]
-  ,
-  [
-    "message" => "Je suis un deuxieme tableau associatif",
-    "info" => [
-      "nombre" => 30,
-      "tableau" => [1, 2, 3]
-    ]
-  ]
-];
-
-// A route with a callback
-get('/api/callback', function () {
-  $info = [
-    "message" => "Je suis un tableau associatif",
-    "info" => [
-      "nombre" => 42,
-      "tableau" => [10, 20, 30]
-    ]
-    ,
-    "message2" => "Je suis un deuxieme tableau associatif",
-    "info2" => [
-      "nombre" => 30,
-      "tableau" => [1, 2, 3]
-    ]
-  ];
-
-});
-
-// A route with a callback passing a variable
-// To run this route, in the browser type:
-// http://localhost/user/A
-get('/api/info/$no', function ($no) {
-  global $info;
-  header('Content-Type: application/json');
-  echo json_encode($info[$no]);
-});
-
-get('/api/info/$no/message', function ($no) {
-  global $info;
-  header('Content-Type: application/json');
-  echo json_encode($info[$no]['message']);
-});
-
-// Route where the query string happends right after a forward slash
-get('/product', '');
-
-// A route with a callback passing 2 variables
-// To run this route, in the browser type:
-// http://localhost/callback/A/B
-get('/callback/$name/$last_name', function ($name, $last_name) {
-  echo "Callback executed. The full name is $name $last_name";
-});
-
-// ##################################################
-// ##################################################
-// ##################################################
-// Route that will use POST data
-post('/user', '/api/save_user');
-
-
-
-// ##################################################
-// ##################################################
-// ##################################################
-// any can be used for GETs or POSTs
-
-// // For GET or POST
-// // The 404.php which is inside the views folder will be called
-// // The 404.php has access to $_GET and $_POST
-// any('/404', 'views/404.php');
+any('/404', 'views/404.php');
