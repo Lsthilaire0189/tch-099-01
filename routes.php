c7c1e2265714b59929a299549bfb8bdfaa8a179a
theobrogrammer <theogeotrif@gmail.com>
2024-04-11 01:48:47 -0400

    ptn

--- routes.php
+++ routes.php
@@ -103,8 +103,8 @@ post('/projet1/api/filtrer', function(){
   $origine = isset($data['origine']) ? $data['origine'] : null;
 $regime = isset($data['regime']) ? $data['regime'] : null;
 $type = isset($data['type']) ? $data['type'] : null;
-header('Content-type: application/json');
-echo json_encode(["message" => "origine: $origine, regime: $regime, type: $type"]);
+//header('Content-type: application/json');
+//echo json_encode(["message" => "origine: $origine, regime: $regime, type: $type"]);
 $conditions = [];
 $params = [];
 
@@ -148,8 +148,8 @@ post('/projet1/api/filtrerUser',function(){
   $requete = $pdo->prepare("SELECT * FROM EQ1_Recette WHERE email = ?");
   $requete->execute([$email]);
   $results= $requete->fetchAll();
- // header('Content-type: application/json');
-//  echo json_encode($results);
+ header('Content-type: application/json');
+ echo json_encode($results);
 });
 
 
